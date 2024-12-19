53923216998fc3519b2bef4b00fc2cba858c1f74
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2024-12-19 17:39:43 +0700

    Add events

---
+++ core/include/userver/tracing/span_event_fwd.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+/// @file userver/tracing/span_event_fwd.hpp
+/// @brief @copybrief tracing::SpanEvent
+
+USERVER_NAMESPACE_BEGIN
+
+namespace tracing {
+
+struct SpanEventAttribute;
+
+struct SpanEvent;
+
+}  // namespace tracing
+
+USERVER_NAMESPACE_END
