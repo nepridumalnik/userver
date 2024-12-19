405bc2819f683ce3d0e93fb8ac0985e866083e7f
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2024-12-19 17:39:43 +0700

    Add events

--- core/src/tracing/span_impl.hpp
+++ core/src/tracing/span_impl.hpp
@@ -15,6 +15,7 @@
 #include <userver/logging/log_helper.hpp>
 #include <userver/tracing/scope_time.hpp>
 #include <userver/tracing/span.hpp>
+#include <userver/tracing/span_event_fwd.hpp>
 #include <userver/tracing/tracer.hpp>
 #include <userver/utils/impl/source_location.hpp>
 
